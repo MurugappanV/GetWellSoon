259f687aceb9dbdd2f73e5db9126bdf0f482b2ab
Unknown <murugappanviswanathan@gmail.com>
2018-05-09 20:36:16 +0530

    Backend - addprescription error solved

--- backend/GetWellSoon/src/prescription/addPrescription/addPrescription.ts
+++ backend/GetWellSoon/src/prescription/addPrescription/addPrescription.ts
@@ -12,20 +12,11 @@ interface EventData {
 
 interface Pharmacy {
     id: string
-    noOfOrders: number 
     orderLabelPrefix: string
-}
-
-// interface Orders {
-//     list: Order[]
-// }
-
-interface Result {
-    id: string
+    noOfOrders: number 
 }
 
 export default async (event: FunctionEvent<EventData>) => {
-    console.log(event)
     try {
         const graphcool = fromEvent(event)
         const api = graphcool.api('simple/v1')
@@ -36,82 +27,27 @@ export default async (event: FunctionEvent<EventData>) => {
         if(!(userId.length > 0)) {
             return { error: `No logged in user - ${event.context.auth}` };
         }
-        let orderInfo: Pharmacy = await getOrderInfo(api, "cjf7zjue562d40118n0cpj3l0").then(r => r.Pharmacy);
-        console.log("phar - ", orderInfo)
+        let orderDate = new Date();
+        const orderInfo: Pharmacy = await getPharmacyOrder(api, "cjf7zjue562d40118n0cpj3l0").then(r => r.Pharmacy)
         let orderId: string = orderInfo.noOfOrders+"";
         let nextOrderId: number = orderInfo.noOfOrders+1;
         for(let i = orderId.length; i <= 6; i++) {
             orderId = "0" + orderId;
         }
         orderId = orderInfo.orderLabelPrefix + orderId;
-        let orderDate = new Date();
-        let result: Result = await savePrescription(api, orderId, orderDate.toISOString(), event.data.billUrl, event.data.message, event.data.isConfirmed, userId, event.data.deliveryAddress,event.data.deliveryPhoneNumber,event.data.deliveryName).then(r => r.Result);
-        if(!(result.id != null && result.id.length > 0)) {
+        const result: string = await addPres(api, orderId, orderDate.toISOString(), event.data.billUrl, event.data.message, true, userId, event.data.deliveryAddress,event.data.deliveryPhoneNumber,event.data.deliveryName, "Order placed by customer");
+        if(!(result != null && result.length > 0)) {
             return { error: `Prescription save error ` };
         }
         await updateOrderInfo(api, orderInfo.id, nextOrderId);
-        //let userId: string | null = null
-
-        return { data: { id: userId} };
+        return { data: { id: orderId} };
     } catch (e) {
         console.log(e)
         return { error: `An unexpected error occured while adding prescription. ${e}` }
     }
 }
 
-
-async function getOrderInfo(api: GraphQLClient,id: string): Promise<{ Pharmacy }> {
-    const query = `
-      query Pharmacy($id: ID!) {
-        Pharmacy(id: $id) {
-            id
-            orderLabelPrefix
-            noOfOrders
-        }
-      }
-    `
-  
-    const variables = {
-        id,
-    }
-  
-    return api.request<{ Pharmacy }>(query, variables)
-}
-
-async function savePrescription(api: GraphQLClient,orderId: string,orderDate: string,billUrl: string,message: string,isConfirmed: boolean,userId: string, deliveryAddress: string,deliveryPhoneNumber: string,deliveryName: string): Promise<{ Result }> {
-    const query = `
-      mutation createPrescription($orderId: String!,$orderDate: DateTime!,$billUrl: String!,$message: String!,$isConfirmed: Boolean!,$userId: ID!,$deliveryAddress: String!,$deliveryPhoneNumber: String!,$deliveryName: String!) {
-        createPrescription(orderId: $orderId, 
-            orderDate:$orderDate,
-            billUrl: $billUrl,
-            customerMessage: $message,
-            isConfirmed: $isConfirmed,
-            customerId: $userId,
-            deliveryAddress: $deliveryAddress,
-            deliveryPhoneNumber: $deliveryPhoneNumber,
-            deliveryName: $deliveryName,
-            logs:[{message: $message, status: "PLACED", action: "Order placed by customer", userId: $userId}]) {
-            id
-        }
-      }
-    `
-    
-    const variables = {
-        orderId,
-        orderDate,
-        billUrl,
-        message,
-        isConfirmed,
-        userId,
-        deliveryAddress,
-        deliveryPhoneNumber,
-        deliveryName,
-    }
-  
-    return api.request<{ Result }>(query, variables)
-}
-
-async function updateOrderInfo(api: GraphQLClient,id: string, noOfOrders: number): Promise<{ Result }> {
+async function updateOrderInfo(api: GraphQLClient,id: string, noOfOrders: number): Promise<{ updatePharmacy }> {
     const query = `
         mutation updatePharmacy($id: ID!,$noOfOrders: Int!) {
             updatePharmacy(id: $id, noOfOrders: $noOfOrders) {
@@ -125,23 +61,48 @@ async function updateOrderInfo(api: GraphQLClient,id: string, noOfOrders: number
         noOfOrders,
     }
   
-    return api.request<{ Result }>(query, variables)
+    return api.request<{ updatePharmacy }>(query, variables)
 }
 
-
-
-// async function getGraphcoolUser(api: GraphQLClient, id: string): Promise<{ User }> {
-//     const query = `
-//       query getUser($id: String!) {
-//         User(id: $id) {
-//           id
-//         }
-//       }
-//     `
+async function getPharmacyOrder(api: GraphQLClient, id: string): Promise<{ Pharmacy }> {
+    const query = `
+      query Pharmacy($id: ID!) {
+        Pharmacy(id: $id) {
+            id
+            orderLabelPrefix
+            noOfOrders
+        }
+      }
+    `
   
-//     const variables = {
-//         id,
-//     }
+    const variables = {
+      id,
+    }
+    return api.request<{ Pharmacy }>(query, variables)
+}
+
+async function addPres(api: GraphQLClient, orderId: string,orderDate: string,billUrl: string,message: string,isConfirmed: boolean,userId: string, deliveryAddress: string,deliveryPhoneNumber: string,deliveryName: string, action: string): Promise<string> {
+    const query = `
+      mutation createPrescription($orderId: String!,$orderDate: String!,$billUrl: String!,$message: String!,$isConfirmed: Boolean!,$userId: ID!,$deliveryAddress: String!,$deliveryPhoneNumber: String!,$deliveryName: String!, $action: String!) {
+        createPrescription(orderId: $orderId, orderDate:$orderDate, billUrl: $billUrl, customerMessage: $message, isConfirmed: $isConfirmed, customerId: $userId, deliveryAddress: $deliveryAddress, deliveryPhoneNumber: $deliveryPhoneNumber, deliveryName: $deliveryName, logs: {message: $message, status: PLACED,action: $action, userId: $userId}) {
+            id
+        }
+      }
+    `
   
-//     return api.request<{ User }>(query, variables)
-//   }
+    const variables = {
+        orderId,
+        orderDate,
+        billUrl,
+        message,
+        isConfirmed,
+        userId,
+        deliveryAddress,
+        deliveryPhoneNumber,
+        deliveryName,
+        action
+    }
+
+    let result: string = await api.request<{ createPrescription }>(query, variables).then(r => r.createPrescription.id).catch(err => console.log(err))
+    return result
+}
